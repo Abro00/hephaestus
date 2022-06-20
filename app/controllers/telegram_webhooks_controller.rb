733c98b175350489396c6d8b18b6febc7a76b175
Абросимов Никита <nikita.abrosimov.2000@gmail.com>
2022-06-20 13:33:43 +0300

    Rubocop corrections

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -1,5 +1,4 @@
 class TelegramWebhooksController < Telegram::Bot::UpdatesController
-  
   def new!(*)
     header, body = parse_header_and_body(payload['text'])
     response = "Created new Jira task with\n\nheader: #{header}\n\nbody: #{body}"
@@ -14,8 +13,7 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
 
   def parse_header_and_body(message)
     body = message.partition("\n\n")[2]
-    header = message.partition("\n\n")[0].partition(" ")[2]
+    header = message.partition("\n\n")[0].partition(' ')[2]
     [header, body]
   end
-
 end
