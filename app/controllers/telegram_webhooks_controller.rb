94bfc1aab553680ff8c13745204929f24d0605d7
Абросимов Никита <nikita.abrosimov.2000@gmail.com>
2022-06-22 15:38:48 +0300

    Add bot mentions, issue's creation time

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -1,32 +1,26 @@
 class TelegramWebhooksController < Telegram::Bot::UpdatesController
   
-  # TODO: отклик на команду по упоминанию бота в группе
-  def new!(*)
-    header, body = parse_header_and_body(payload['text'])
+  def message(message)
+    if message['text'].start_with?("@#{Telegram.bot.username}")
+      summary, description = parse_summary_and_description(message['text'])
 
-    options = {
-      username:     Rails.application.credentials.dig(:jira, :username),
-      password:     Rails.application.credentials.dig(:jira, :api_token),
-      site:         Rails.application.credentials.dig(:jira, :site),
-      context_path: '',
-      auth_type:    :basic
-    }
-    client = JIRA::Client.new(options)
-
-    issue = client.Issue.build
-    issue.save(
-      {
-        'fields' => {
-          'summary'     => header,
-          'project'     => { 'key' => 'GEGE' },
-          'description' => body,
-          'issuetype'   => { 'name' => 'Task' }
-        }
+      options = {
+        username:     Rails.application.credentials.dig(:jira, :username),
+        password:     Rails.application.credentials.dig(:jira, :api_token),
+        site:         Rails.application.credentials.dig(:jira, :site),
+        context_path: '',
+        auth_type:    :basic
       }
-    )
+      client = JIRA::Client.new(options)
 
-    response = "Создана задача с номером [#{issue.key}](#{client.request_client.options[:site]}browse/#{issue.key})"
-    reply_with :message, text: response, parse_mode: "Markdown"
+      issue, saved = save_issue(client, message, summary, description)
+      if saved
+        response = "Создана задача с номером [#{issue.key}](#{client.request_client.options[:site]}browse/#{issue.key})"
+        reply_with :message, text: response, parse_mode: 'Markdown'
+      else
+        reply_with :message, text: 'Не удалось создать задачу'
+      end
+    end
   end
 
   def start!(*)
@@ -35,9 +29,24 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
 
   private
 
-  def parse_header_and_body(message)
-    body = message.partition("\n\n")[2]
-    header = message.partition("\n\n")[0].partition(' ')[2]
-    [header, body]
+  def parse_summary_and_description(message)
+    description = message.partition("\n\n")[2]
+    summary = message.partition("\n\n")[0].partition(' ')[2]
+    [summary, description]
+  end
+
+  def save_issue(client, message, summary, description)
+    issue = client.Issue.build
+    description<<"\n\nСоздано @#{message['from']['username']} в \"#{message['chat']['title']}\", [#{Time.at(message['date']).strftime("%R %d.%m.%y")}]"
+    [issue, issue.save(
+      {
+        'fields' => {
+          'summary'     => summary,
+          'project'     => { 'key' => 'GEGE' },
+          'description' => description,
+          'issuetype'   => { 'name' => 'Task' }
+        }
+      }
+    )]
   end
 end
