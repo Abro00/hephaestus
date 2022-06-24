7a05dd76d2a14b5c8c9742666f705e90df2f774c
Абросимов Никита <nikita.abrosimov.2000@gmail.com>
2022-06-24 16:13:29 +0300

    RUbocop corrections

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(_resource)
     user_path(current_user)
   end
 end
--- test/models/connection_test.rb
+++ test/models/connection_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class ConnectionTest < ActiveSupport::TestCase
   # test "the truth" do
