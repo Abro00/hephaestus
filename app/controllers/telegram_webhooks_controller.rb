580d0f3545724e25bb8997a0fee3e617ddc5ff87
Абросимов Никита <nikita.abrosimov.2000@gmail.com>
2022-06-25 22:57:02 +0300

    Add help command and notification if not connected

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -1,6 +1,7 @@
 class TelegramWebhooksController < Telegram::Bot::UpdatesController
   I18n.locale = 'bot'
 
+  # adding bot to a new group
   def my_chat_member(my_chat_member)
     if my_chat_member['new_chat_member']['status'] == 'member'
       respond_with :message, text: t('chat_ivite')
@@ -10,20 +11,24 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
     Chat.find(my_chat_member['chat']['id']).destroy if my_chat_member['new_chat_member']['status'] == 'left'
   end
 
+  # request to create issue
   def message(message)
     return if message['text'].nil?
     return unless message['text'].start_with?("@#{Telegram.bot.username}")
 
+    if message['chat']['type'] == 'private'
+      respond_with :message, text: t('private_mention')
+      return
+    end
+
+    unless Connection.exists?(chat_id: message['chat']['id'])
+      respond_with :message, text: t('no_connection')
+      return
+    end
+
     summary, description = parse_summary_and_description(message['text'])
 
-    options = {
-      username:     Rails.application.credentials.dig(:jira, :username),
-      password:     Rails.application.credentials.dig(:jira, :api_token),
-      site:         Rails.application.credentials.dig(:jira, :site),
-      context_path: '',
-      auth_type:    :basic
-    }
-    client = JIRA::Client.new(options)
+    client = set_jira_client
 
     issue, saved = save_issue(client, message, summary, description)
     if saved
@@ -39,10 +44,21 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
   end
 
   def help!(*)
-    respond_with :message, text: t('help')
+    respond_with :message, text: t('help', botname: Telegram.bot.username), parse_mode: 'Markdown'
   end
 
   private
+
+  def set_jira_client
+    options = {
+      username:     Rails.application.credentials.dig(:jira, :username),
+      password:     Rails.application.credentials.dig(:jira, :api_token),
+      site:         Rails.application.credentials.dig(:jira, :site),
+      context_path: '',
+      auth_type:    :basic
+    }
+    JIRA::Client.new(options)
+  end
 
   def parse_summary_and_description(message)
     description = message.partition("\n\n")[2]
