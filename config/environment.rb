2cab0694f4e514f44c3d2791355cb2f103341c66
Абросимов Никита <nikita.abrosimov.2000@gmail.com>
2022-07-03 17:28:42 +0300

    Update README

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,5 @@ require_relative "application"
 Rails.application.initialize!
 
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  html_tag.gsub("form-control", "form-control is-invalid").html_safe
+  html_tag.gsub("form-control", "form-control is-invalid").gsub("form-select", "form-select is-invalid").html_safe
 end
